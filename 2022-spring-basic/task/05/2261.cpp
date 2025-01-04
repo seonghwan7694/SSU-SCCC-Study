5f35b0f559323ee539178ce3bb87ff6891ca9b93
seonghwan7694 <seonghwan7694@gamil.com>
2025-01-04 23:32:36 +0900

    2261번 스위핑 문제 풀기

---
+++ 2022-spring-basic/task/05/2261.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pii pair<int, int>
+#define x first
+#define y second
+
+int n;
+vector<pii> v;
+int main(){
+  ios::sync_with_stdio(false);
+
+  cin >> n;
+  for(int i = 0, a, b; i < n; i++){
+
+  }
+
+
+  return 0;
+}
