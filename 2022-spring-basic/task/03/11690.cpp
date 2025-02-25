b4fbef709bafe22256b05174d79216f4d6d354ae
seonghwan7694 <seonghwan7694@gmail.com>
2025-02-25 22:10:52 +0900

    solved 2824

--- 2022-spring-basic/task/03/11690.cpp
+++ 2022-spring-basic/task/03/11690.cpp
@@ -1,13 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
+#define MOD 1'000'000'000ll
 
-ll n;
+ll N, M, tmp; // 1 <= N <= 1e12
+map<ll, ll> nm, mm;
+
+void func(map<ll, ll> &m, ll v){
+  for(ll i = 2; i <= sqrt(v); i++){
+    if(v % i == 0) m[i]++;
+  }
+
+}
 
 int main(){
-  ios::sync_with_stdio(false);
-  cin >> n;
-
-
+    ios::sync_with_stdio(false);
+  cin >> N;
+  for(int i = 0; i < N; i++){
+    cin >> tmp;
+    func(nm, tmp);
+  }
+  cin >> M;
+  for(int i = 0; i < M; i++){
+    cin >> tmp;
+  }
   return 0;
 }
--- 2022-spring-basic/task/03/2824.cpp
+++ 2022-spring-basic/task/03/2824.cpp
@@ -1,28 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-#define MOD 1'000'000'000ll
-
-ll N, M, tmp; // 1 <= N <= 1e12
+#define MAX_NUM 1'000'000'000ll
+ll N, M, ans = 1, tmp;
+bool flag = false;
 map<ll, ll> nm, mm;
 
+// 최대 공약수 -> 두 수에서 공통적으로 가지고 있는 소수
+
 void func(map<ll, ll> &m, ll v){
-  for(ll i = 2; i <= sqrt(v); i++){
-    if(v % i == 0) m[i]++;
+  for(ll i = 2; i * i <= v; i++){
+    while(v % i == 0){
+      m[i]++;
+      v /= i;
+    }
   }
-
+  if(v != 1) m[v]++;
+  return;
 }
-
 int main(){
-    ios::sync_with_stdio(false);
+  ios::sync_with_stdio(false);
   cin >> N;
-  for(int i = 0; i < N; i++){
+  while(N--){
     cin >> tmp;
     func(nm, tmp);
   }
   cin >> M;
-  for(int i = 0; i < M; i++){
+  while(M--){
     cin >> tmp;
+    func(mm, tmp);
   }
+  for(auto iter = nm.begin(); iter != nm.end(); iter++){
+    auto iter2 = mm.find(iter->first);
+    auto v = min(iter->second, iter2->second);
+    if(iter2 == mm.end()) continue;
+    while(v--){
+      ans *= iter->first;
+      if(ans >= MAX_NUM) ans %= (ll) MAX_NUM, flag = true;
+    }
+  }
+
+  if(flag){
+    string str = to_string(ans);
+    for(int i = 0; i < 9 - str.length(); i++) cout << "0";
+  }
+  cout << ans << "\n";
   return 0;
 }
