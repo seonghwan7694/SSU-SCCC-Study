03c4b776114178adcef58c30965e968a8823bdab
seonghwan7694 <seonghwan7694@gmail.com>
2025-02-27 02:15:37 +0900

    solved 2616

--- 2022-spring-basic/task/04/1334.cpp
+++ 2022-spring-basic/task/04/1334.cpp
@@ -4,6 +4,7 @@ using namespace std;
 string s;
 int dp[51][51][10]; // dp[r][c][v] =
 
+// n < 10^51
 int main(){
   ios::sync_with_stdio(false);
   cin >> s;
--- 2022-spring-basic/task/04/2616.cpp
+++ 2022-spring-basic/task/04/2616.cpp
@@ -1,19 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-int N, n;
-vector<int> A;
-
+// n log n -> 누적 합을 저장해두기
+// n^3 순간 망해버림
+int N, tmp, M, SUM[20202], dp[4][50505]; // dp[i][n] = max(dp[i][n-1], dp[i-1][j-K] + SUM[j] - SUM[j-K]
 int main(){
   ios::sync_with_stdio(false);
   cin >> N;
-  A.resize(N+1);
   for(int i = 1; i <= N; i++){
-    cin >> A[i];
+    cin >> tmp;
+    SUM[i] = SUM[i-1] + tmp;
   }
-  cin >> n;
-
-
-
-
+  cin >> M;
+  for(int i = 1; i <= 3; i++){
+    for(int j = i * M; j <= N; j++){
+      dp[i][j] = max(dp[i][j-1], dp[i-1][j-M] + SUM[j] - SUM[j-M]);
+    }
+  }
+  cout << dp[3][N] << "\n";
   return 0;
 }
